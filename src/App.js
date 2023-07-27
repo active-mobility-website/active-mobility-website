1c00b01e556a87cf95b5b5dddbad4a5d0af32da3
ryanfic <ryan_vincent_ficocelli@hotmail.com>
2023-07-26 22:35:19 -0700

    Initial deploy website

--- src/App.js
+++ src/App.js
@@ -6,9 +6,6 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
         <a
           className="App-link"
           href="https://reactjs.org"
@@ -17,7 +14,7 @@ function App() {
         >
           Learn React
         </a>
-        <div>This is for testing purposes</div>
+        <div>Hopefully this deployed</div>
       </header>
     </div>
   );
